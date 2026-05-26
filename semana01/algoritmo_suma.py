832703ae2755d682933df5638c51e4a174048e12
akemimarleny <24425340+AkemiMarleny@users.noreply.github.com>
2026-05-26 18:00:03 -0500

    docs: aggiungi commenti all algoritmo somma

--- semana01/algoritmo_suma.py
+++ semana01/algoritmo_suma.py
@@ -1,3 +1,12 @@
+#Algoritmo: Sommare due numeri
+# 1. Definire le variabili per i numeri (num1, num2)
+# 2. Richiedere all'utente di inserire il primero numero
+# 3. Richiedere all'utente di inserire il secondo numero
+# 4. Calcolare la somma: risultato = num1 + num2
+# 5. Mostrare il risultato all'utente
+
+#Implementazione in Python:
+
 num1 = float(input("Inserisce il primo numero: "))
 num2 = float(input("Inserisce il secondo numero: "))
 
