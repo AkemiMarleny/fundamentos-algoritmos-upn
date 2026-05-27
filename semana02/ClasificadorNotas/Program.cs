51e64d61fb481da81c88127552ed6d68b5519681
akemimarleny <24425340+AkemiMarleny@users.noreply.github.com>
2026-05-26 21:34:26 -0500

    feat: clasificador notas

---
+++ semana02/ClasificadorNotas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Console.WriteLine("CLASIFICADOR DE NOTAS");
+        Console.Write("Ingrese la nota del estudiante: ");
+        string input = Console.ReadLine();
+        if (double.TryParse(input, out double voto))
+        {
+            ClasificarNota(voto);
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Ingrese un número.");
+        }
+
+        static void ClasificarNota(double voto)
+    {
+        
+        if (voto < 0 || voto > 20)
+        {
+            Console.WriteLine("Error: La calificación debe estar comprendida entre 0 y 20.");
+        }
+        else if (voto == 20)
+        {
+            Console.WriteLine("Excelente");
+        }
+        else if (voto >= 11)
+        {
+            Console.WriteLine("Aprobado");
+        }
+        else
+        {
+            Console.WriteLine("Desaprobado");
+        }
+    }
+
