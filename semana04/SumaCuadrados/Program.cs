0dfc6b2cb9d7f07839d072018e3a68858cc85f30
AkemiMarleny <24425340+AkemiMarleny@users.noreply.github.com>
2026-06-24 14:50:24 -0500

    feat: suma cuadrados

---
+++ semana04/SumaCuadrados/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.ComponentModel;
+
+void Main()
+{
+
+    int numero = 0;
+    do
+    {
+        Console.Write("Ingresa un número: ");
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out numero) && numero <= 0)
+        {
+            Console.WriteLine("El valor ingresado, no está permitido");
+        }
+    }
+    while (numero <= 0);
+
+    int somma = 0;
+    for (int i = 1; i <= numero; i++)
+    {
+        int quadrato = i * i;
+        somma += quadrato;
+        if (i == 1)
+        {
+            Console.Write($"S = {i}*{i} ");
+        }
+        else
+        {
+
+            Console.Write($"+ {i}*{i} ");
+        }
+    }
+
+    Console.WriteLine($"\nLa suma total de la serie es {somma}");
+}
+
+Main();
